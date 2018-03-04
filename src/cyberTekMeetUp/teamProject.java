af3e5cd5269d3911563269dd1d3662d3b6ee90d2
altynbekabdurasulov <altynbekabdurasulov@10.1.10.223>
2018-03-04 15:59:37 -0600

    teamProject

---
+++ src/cyberTekMeetUp/teamProject.java
@@ -0,0 +1,14 @@
+package cyberTekMeetUp;
+import javax.swing.JOptionPane;
+public class teamProject {
+	public static void main(String[] args) {
+		String f1 = JOptionPane.showInputDialog("Enter username");
+		String f2 = JOptionPane.showInputDialog("Enter password");
+
+		JOptionPane.showInputDialog(f1, f2);
+	}
+
+}
+
+
+
